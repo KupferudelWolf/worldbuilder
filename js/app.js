2f2e5ea123fca930aa793aa1c2b5ef00ae0bd663
Avoyt <avoytdesign@gmail.com>
2021-04-21 05:34:59 -0400

    Improved click-and-drag.

--- js/app.js
+++ js/app.js
@@ -37,15 +37,18 @@ console.log(Date.now().toString(32));
 
 (function () {
   $(document).ready(function () {
-    let frame = $('.bubble-frame');
-    let container = $('.bubble-container');
-    let cont = {
-      // top: 0,
-      bottom: 0,
-      // left: 0,
-      right: 0
-    }
-    let focus;
+    const frame = $('.bubble-frame'),
+          container = $('.bubble-container'),
+          cont = {
+            // top: 0,
+            bottom: 0,
+            // left: 0,
+            right: 0
+          };
+
+    let defaultFramePosition;
+
+    var dragging;
 
     DATA.forEach((obj, ind) => {
       let bubble = $('<div>')
@@ -65,42 +68,43 @@ console.log(Date.now().toString(32));
       cont.right  = Math.max(cont.right, obj.bubble.x + bubble.outerWidth() + frame.height()/2);
 
       if (obj.focus) {
-        focus = [
+        defaultFramePosition = [
           obj.bubble.x + bubble.outerWidth()/2,
           obj.bubble.y + bubble.outerHeight()/2
         ];
       }
 
-      let startX, startY, left, top, dragging, moved;
+      let startX, startY, moved;
       bubble
         .on('mousedown', function (e) {
-          e.preventDefault();
+          if (dragging) return;
+          e.stopPropagation();
           const self = $(this);
-          left = self.position().left;
-          top = self.position().top;
+          dragging = {
+            self: self,
+            e: e
+          };
           startX = e.pageX;
           startY = e.pageY;
-          dragging = true;
           moved = false;
-          self.addClass('active');
+          frame.trigger('mousedown');
+          // return false;
         })
         .on('mousemove', function (e) {
           if (!dragging) return;
           const self = $(this);
-          e.preventDefault();
           let dX = e.pageX - startX,
-              dY = e.pageY - startY,
-              rate = 1;
-          self.css('left', dX * rate + left + 'px');
-          self.css('top',  dY * rate + top + 'px');
+              dY = e.pageY - startY;//,
+              // rate = 1;
+          // self.css('left', dX * rate + left + 'px');
+          // self.css('top',  dY * rate + top + 'px');
           if (Math.abs(dX) >= 1 || Math.abs(dY) >= 1) {
             moved = true;
           }
           self.trigger('bubble:onmove');
         })
         .on('mouseup mouseleave', function () {
-          if (!dragging) return;
-          // console.log(moved);
+          // if (!dragging) return;
           const self = $(this);
           let left = self.css('left').slice(0, -2),
               top = self.css('top').slice(0, -2);
@@ -108,38 +112,45 @@ console.log(Date.now().toString(32));
           top = Math.min(Math.max(top, 0), container.height() - self.height());
           self.css('left', left + 'px');
           self.css('top',  top + 'px');
-          dragging = false;
-          self.removeClass('active');
+          // dragging = false;
+          // self.removeClass('active');
+          self.trigger('bubble:onmove');
         });
 
       obj.div = bubble;
     });
 
     let updateLineFunction = function (elems, line) {
-      let pos = [],
-          rad = [],
-          attr = {},
-          angle;
+      let attr = {}, pos = [], rad = [], dist, angle, radii = 0;
 
       for (let i = 0; i < 2; ++i) {
-        let e = elems[i],
-            p = e.position();
+        let e = elems[i];
+        pos[i] = elems[i].position();
         rad[i] = [
           e.outerWidth()/2,
           e.outerHeight()/2
         ];
-        pos[i] = [
-          p.left + rad[i][0],
-          p.top + rad[i][1]
-        ];
+        radii += Math.max(...rad[i]);
+        pos[i].left += rad[i][0];
+        pos[i].top += rad[i][1];
       }
 
-      angle = Math.atan2(pos[1][1] - pos[0][1], pos[1][0] - pos[0][0]);
+      dist = Math.sqrt((pos[1].top - pos[0].top)**2 + (pos[1].left - pos[0].left)**2);
+
+      if (dist <= radii) {
+        line.attr({
+          x1: 0, x2: 0,
+          y1: 0, y2: 0
+        });
+        return;
+      }
+
+      angle = Math.atan2(pos[1].top - pos[0].top, pos[1].left - pos[0].left);
 
       for (let i = 0; i < 2; ++i) {
         let sgn = i ? -1 : 1,
-            x = pos[i][0] + sgn * Math.cos(angle) * rad[i][0],
-            y = pos[i][1] + sgn * Math.sin(angle) * rad[i][1];
+            x = pos[i].left + sgn * Math.cos(angle) * rad[i][0],
+            y = pos[i].top + sgn * Math.sin(angle) * rad[i][1];
         attr[`x${i+1}`] = x;
         attr[`y${i+1}`] = y;
       }
@@ -172,51 +183,72 @@ console.log(Date.now().toString(32));
     let cW = cont.right /*- cont.left*/,
         cH = cont.bottom /*- cont.top*/;
 
-    if (!focus) {
-      focus = [cW/2, cH/2];
+    if (!defaultFramePosition) {
+      defaultFramePosition = [cW/2, cH/2];
     }
 
     container.css({
       'width':  cW + 'px',
       'height': cH + 'px',
-      'left': -focus[0] + frame.width()/2 - parseInt(container.css('margin-left')),
-      'top':  -focus[1] + frame.height()/2 - parseInt(container.css('margin-top'))
+      'left': -defaultFramePosition[0] + frame.width()/2 - parseInt(container.css('margin-left')),
+      'top':  -defaultFramePosition[1] + frame.height()/2 - parseInt(container.css('margin-top'))
     });
 
 
 
-    controlContainerDrag();
+    controlDrag();
 
 
-    function controlContainerDrag() {
-      let startX, startY, left, top, dragging;
+    function controlDrag() {
+      let startX, startY, left, top, rate;
       container
         .on('mousedown', (e) => {
-          left = container.position().left;
-          top = container.position().top;
-          startX = e.pageX;
-          startY = e.pageY;
-          dragging = true;
-          container.addClass('active');
-        })
-        .on('mousemove', (e) => {
-          if (!dragging || $('.bubble.active').length) return;
-          e.preventDefault();
-          container.css('left', (e.pageX - startX) * 0.75 + left + 'px');
-          container.css('top',  (e.pageY - startY) * 0.75 + top + 'px');
+          if (dragging) return;
+          dragging = {
+            self: container,
+            rate: 0.75
+          };
         })
         .on('mouseup mouseleave', () => {
-          if (!dragging || $('.bubble.active').length) return;
+          if (!dragging) return;
           let left = container.css('left').slice(0, -2),
               top = container.css('top').slice(0, -2);
           left = Math.min(Math.max(left, frame.width() - container.outerWidth(true)), 0);
           top = Math.min(Math.max(top, frame.height() - container.outerHeight(true)), 0);
           container.css('left', left + 'px');
           container.css('top',  top + 'px');
-          dragging = false;
-          container.removeClass('active');
         });
-      }
+
+      frame
+        .on('mousedown', (e) => {
+          if (!dragging) return;
+          e = dragging.e || e;
+          e.preventDefault();
+
+          left = dragging.self.position().left;
+          top = dragging.self.position().top;
+          startX = e.pageX;
+          startY = e.pageY;
+          rate = dragging.rate || 1;
+          dragging.self.addClass('active');
+        })
+        .on('mousemove', (e) => {
+          if (!dragging) return;
+          e.preventDefault();
+
+          let dX = e.pageX - startX,
+              dY = e.pageY - startY;
+          dragging.self.css('left', dX * rate + left + 'px');
+          dragging.self.css('top',  dY * rate + top + 'px');
+        })
+        .on('mouseup mouseleave', (e) => {
+          if (!dragging) return;
+          e.preventDefault();
+
+          dragging.self.removeClass('active');
+          dragging = false;
+        });
+    }
 
     //
 
