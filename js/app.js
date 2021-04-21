ec22423be09bea249eca2919f41466c8fcb96be0
Avoyt <avoytdesign@gmail.com>
2021-04-21 07:17:26 -0400

    Cleanup and documentation.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
+/// Temporary.
 const DATA = [
   {
     class: 'character',
     title: 'Abigail',
     desc: '',
-    id: 'abigail-1f3ohguup',
     focus: true,
     bubble: {
       x: 500,
@@ -33,94 +33,141 @@ const DATA = [
   }
 ];
 
+/// Temporary.
 console.log(Date.now().toString(32));
 
 (function () {
-  $(document).ready(function () {
-    const frame = $('.bubble-frame'),
-          container = $('.bubble-container'),
-          cont = {
-            // top: 0,
-            bottom: 0,
-            // left: 0,
-            right: 0
-          };
 
-    let defaultFramePosition;
+    var _dragging, BUBBLE_FRAME, BUBBLE_CONTAINER;
 
-    var dragging;
+    const SVG = $(document.createElementNS('http://www.w3.org/2000/svg','svg')),
+          /// DICT holds all bubbles / entries.
+          DICT = {
+            /// Redundantly contain all entries in _.
+            _: [],
+            /// Add a new bubble to the dictionary.
+            add: function (bubble) {
+              DICT._.push(bubble);
+              let key = bubble.class;
+              if (typeof(key) !== 'string') return;
+              if (!DICT[key]) DICT[key] = [];
+              DICT[key].push(bubble);
+            },
+            /// Select all bubbles of specific classes.
+            /// Excluded classes with an exclamation mark.
+            select: function (args) {
+              let out = [], whitelist, blacklist;
+              if (typeof(args) === 'string') args = [args];
 
-    DATA.forEach((obj, ind) => {
-      let bubble = $('<div>')
-            .addClass('bubble')
-            .addClass(obj.class)
-            .attr('id', obj.id)
-            .css({
-              'left': obj.bubble.x + 'px',
-              'top': obj.bubble.y + 'px'
-            })
-            .html(`<p>${obj.title}</p>`)
-            .appendTo(container);
+              whitelist = args.filter(s => s[0] !== '!');
+              blacklist = args.filter(s => s[0] === '!');
+              blacklist.push('!_');
 
-      // cont.top    = Math.max(cont.top, obj.bubble.y);
-      cont.bottom = Math.max(cont.bottom, obj.bubble.y + bubble.outerHeight() + frame.width()/2);
-      // cont.left   = Math.max(cont.left, obj.bubble.x);
-      cont.right  = Math.max(cont.right, obj.bubble.x + bubble.outerWidth() + frame.height()/2);
+              if (whitelist.length <= 0) whitelist = Object.keys(DICT);
 
-      if (obj.focus) {
-        defaultFramePosition = [
-          obj.bubble.x + bubble.outerWidth()/2,
-          obj.bubble.y + bubble.outerHeight()/2
-        ];
-      }
+              return Object.keys(DICT)
+                .filter((key) => {
+                  return true
+                    && typeof(DICT[key]) === 'object'
+                    && whitelist.includes(key)
+                    && !blacklist.includes('!'+key);
+                })
+                .reduce((obj, key) => {
+                  obj[key] = DICT[key];
+                  return obj[key];
+                }, []);
+            }
+          },
 
-      let startX, startY, moved;
-      bubble
-        .on('mousedown', function (e) {
-          if (dragging) return;
-          e.stopPropagation();
-          const self = $(this);
-          dragging = {
-            self: self,
-            e: e
-          };
-          startX = e.pageX;
-          startY = e.pageY;
-          moved = false;
-          frame.trigger('mousedown');
-          // return false;
-        })
-        .on('mousemove', function (e) {
-          if (!dragging) return;
-          const self = $(this);
-          let dX = e.pageX - startX,
-              dY = e.pageY - startY;//,
-              // rate = 1;
-          // self.css('left', dX * rate + left + 'px');
-          // self.css('top',  dY * rate + top + 'px');
-          if (Math.abs(dX) >= 1 || Math.abs(dY) >= 1) {
-            moved = true;
-          }
-          self.trigger('bubble:onmove');
-        })
-        .on('mouseup mouseleave', function () {
-          // if (!dragging) return;
-          const self = $(this);
-          let left = self.css('left').slice(0, -2),
-              top = self.css('top').slice(0, -2);
-          left = Math.min(Math.max(left, 0), container.width() - self.width());
-          top = Math.min(Math.max(top, 0), container.height() - self.height());
-          self.css('left', left + 'px');
-          self.css('top',  top + 'px');
-          // dragging = false;
-          // self.removeClass('active');
-          self.trigger('bubble:onmove');
-        });
+          /// Load data from JSON (well, from test data at the moment).
+          loadEntires = function () {
+            let corners = { bottom: 0, right: 0 },
+                defaultFramePosition, startX, startY, left, top, rate;
 
-      obj.div = bubble;
-    });
+            DATA.forEach((obj, ind) => {
+              let bubble = new Bubble(obj);
 
-    let updateLineFunction = function (elems, line) {
+              corners.bottom = Math.max(corners.bottom, bubble.y + bubble.element.outerHeight() + BUBBLE_FRAME.width()/2);
+              corners.right  = Math.max(corners.right, bubble.x + bubble.element.outerWidth() + BUBBLE_FRAME.height()/2);
+
+              if (obj.focus) {
+                defaultFramePosition = [
+                  bubble.x + bubble.element.outerWidth()/2,
+                  bubble.y + bubble.element.outerHeight()/2
+                ];
+              }
+
+              obj.bubble = bubble;
+            });
+
+            if (!defaultFramePosition) {
+              defaultFramePosition = [corners.right/2, corners.bottom/2];
+            }
+
+            BUBBLE_CONTAINER
+              .css({
+                'width':  corners.right + 'px',
+                'height': corners.bottom + 'px',
+                'left': -defaultFramePosition[0] + BUBBLE_FRAME.width()/2 - parseInt(BUBBLE_CONTAINER.css('margin-left')),
+                'top':  -defaultFramePosition[1] + BUBBLE_FRAME.height()/2 - parseInt(BUBBLE_CONTAINER.css('margin-top'))
+              })
+              .on('mousedown', (e) => {
+                if (_dragging) return;
+                _dragging = {
+                  self: BUBBLE_CONTAINER,
+                  rate: 0.75
+                };
+              })
+              .on('mouseup mouseleave', () => {
+                if (!_dragging) return;
+                let left = BUBBLE_CONTAINER.css('left').slice(0, -2),
+                    top = BUBBLE_CONTAINER.css('top').slice(0, -2);
+                left = Math.min(Math.max(left, BUBBLE_FRAME.width() - BUBBLE_CONTAINER.outerWidth(true)), 0);
+                top = Math.min(Math.max(top, BUBBLE_FRAME.height() - BUBBLE_CONTAINER.outerHeight(true)), 0);
+                BUBBLE_CONTAINER.css('left', left + 'px');
+                BUBBLE_CONTAINER.css('top',  top + 'px');
+              });
+          },
+
+          /// Click-and-drag functionality.
+          /// Activates when _dragging is set.
+          controlDrag = function () {
+            let startX, startY, rate, left, top;
+            $('.bubble-frame')
+              .on('mousedown', (e) => {
+                if (!_dragging) return;
+                e = _dragging.e || e;
+                e.preventDefault();
+
+                left = _dragging.self.position().left;
+                top = _dragging.self.position().top;
+                startX = e.pageX;
+                startY = e.pageY;
+                rate = _dragging.rate || 1;
+                _dragging.self.addClass('active');
+              })
+              .on('mousemove', (e) => {
+                if (!_dragging) return;
+                e.preventDefault();
+
+                let dX = e.pageX - startX,
+                    dY = e.pageY - startY;
+                _dragging.self.css({
+                  left: dX * rate + left + 'px',
+                  top:  dY * rate + top  + 'px'
+                });
+              })
+              .on('mouseup mouseleave', (e) => {
+                if (!_dragging) return;
+                e.preventDefault();
+
+                _dragging.self.removeClass('active');
+                _dragging = false;
+              });
+          },
+
+          /// Line manipulation; for Bubble internal use.
+          updateLine = function (elems, line) {
       let attr = {}, pos = [], rad = [], dist, angle, radii = 0;
 
       for (let i = 0; i < 2; ++i) {
@@ -136,8 +183,8 @@ console.log(Date.now().toString(32));
       }
 
       dist = Math.sqrt((pos[1].top - pos[0].top)**2 + (pos[1].left - pos[0].left)**2);
-
       if (dist <= radii) {
+        /// Circles meet or intersect.
         line.attr({
           x1: 0, x2: 0,
           y1: 0, y2: 0
@@ -148,109 +195,124 @@ console.log(Date.now().toString(32));
       angle = Math.atan2(pos[1].top - pos[0].top, pos[1].left - pos[0].left);
 
       for (let i = 0; i < 2; ++i) {
-        let sgn = i ? -1 : 1,
+        let sgn = 1,//i ? -1 : 1,
             x = pos[i].left + sgn * Math.cos(angle) * rad[i][0],
             y = pos[i].top + sgn * Math.sin(angle) * rad[i][1];
-        attr[`x${i+1}`] = x;
-        attr[`y${i+1}`] = y;
+        attr[`x${i+1}`] = Math.round(x);
+        attr[`y${i+1}`] = Math.round(y);
+        angle += Math.PI;
       }
 
-      // for (let key in attr) {
-        // attr[key] += 'px';
-      // }
       line.attr(attr);
     };
 
-    let svgContainer = $(document.createElementNS('http://www.w3.org/2000/svg','svg'));
-    svgContainer.appendTo(container);
-    svgContainer.get(0).classList.add('bubbleSVG');
-    for (let i = 0, l = DATA.length; i < l; ++i) {
-      let obj = DATA[i];
-      if (!obj.events) continue;
-      let divA = obj.div;
-      for (let j = 0, m = obj.events.length; j < m; ++j) {
-        let id = obj.events[j];
-        let divB = $(`#${id}`);
-        if (!divB.length) continue;
-        let line = $(document.createElementNS('http://www.w3.org/2000/svg','line'));
-        svgContainer.append(line);
-        updateLineFunction([divA, divB], line)
-        divA.on('bubble:onmove', () => updateLineFunction([divA, divB], line));
-        divB.on('bubble:onmove', () => updateLineFunction([divA, divB], line));
+    class Bubble {
+      constructor(prop, x, y) {
+        if (!prop.bubble) {
+          prop.bubble = {
+            x: x || 0,
+            y: y || 0
+          }
+        }
+        this.x = prop.bubble.x;
+        this.y = prop.bubble.y;
+        this.events = prop.events || [];
+        this.class = prop.class || 'event';
+        this.title = prop.title || 'New Bubble';
+        this.id = prop.id;
+        if (!this.id) {
+          this.id = this.title
+            .toLowerCase()
+            .replace(/[\s\-]/g, '_')
+            .replace(/[^0-9a-z_]/g, '')
+            + '-' + Date.now().toString(32);
+        }
+
+        DICT.add(this);
+
+        this.element =  $('<div>')
+          .addClass('bubble')
+          .addClass(this.class)
+          .attr('id', this.id)
+          .css({
+            'left': this.x + 'px',
+            'top': this.y + 'px'
+          })
+          .html(`<p>${this.title}</p>`)
+          .appendTo(BUBBLE_CONTAINER);
+
+        let startX, startY, moved;
+        this.element
+          .on('mousedown', function (e) {
+            if (_dragging) return;
+            e.stopPropagation();
+            _dragging = {
+              self: $(this),
+              e: e
+            };
+            startX = e.pageX;
+            startY = e.pageY;
+            moved = false;
+            BUBBLE_FRAME.trigger('mousedown');
+          })
+          .on('mousemove', function (e) {
+            if (!_dragging) return;
+            const self = $(this);
+            let dX = e.pageX - startX,
+                dY = e.pageY - startY;
+            if (Math.abs(dX) >= 1 || Math.abs(dY) >= 1) {
+              moved = true;
+            }
+            self.trigger('bubble:onmove');
+          })
+          .on('mouseup mouseleave', function () {
+            // if (!_dragging) return;
+            const self = $(this);
+            let left = self.css('left').slice(0, -2),
+                top = self.css('top').slice(0, -2);
+            left = Math.min(Math.max(left, 0), BUBBLE_CONTAINER.width() - self.width());
+            top = Math.min(Math.max(top, 0), BUBBLE_CONTAINER.height() - self.height());
+            self.css('left', left + 'px');
+            self.css('top',  top + 'px');
+            // self.trigger('bubble:onmove');
+          });
+
+        if (this.class === 'event') {
+          let targs = DICT.select('!event');
+          console.log(targs);
+          for (let i = 0, l = targs.length; i < l; ++i) {
+            let bubble = targs[i];
+            if (!bubble.events.includes(this.id)) continue;
+            bubble.createLine(this.element);
+          }
+        } else {
+          for (let i = 0, l = this.events.length; i < l; ++i) {
+            let id = this.events[i];
+            let target = $(`#${id}`);
+            if (!target.length) continue;
+            this.createLine(target);
+          }
+        }
+      }
+
+      createLine(targ) {
+        let self = this.element,
+            line = $(document.createElementNS('http://www.w3.org/2000/svg','line'));
+        SVG.append(line);
+        updateLine([self, targ], line)
+        self.on('bubble:onmove', () => updateLine([self, targ], line));
+        targ.on('bubble:onmove', () => updateLine([self, targ], line));
       }
     }
 
-    let cW = cont.right /*- cont.left*/,
-        cH = cont.bottom /*- cont.top*/;
+    $(document).ready(function () {
+      BUBBLE_FRAME = $('.bubble-frame');
+      BUBBLE_CONTAINER = $('.bubble-container');
 
-    if (!defaultFramePosition) {
-      defaultFramePosition = [cW/2, cH/2];
-    }
+      SVG.appendTo(BUBBLE_CONTAINER);
+      SVG.get(0).classList.add('bubbleSVG');
 
-    container.css({
-      'width':  cW + 'px',
-      'height': cH + 'px',
-      'left': -defaultFramePosition[0] + frame.width()/2 - parseInt(container.css('margin-left')),
-      'top':  -defaultFramePosition[1] + frame.height()/2 - parseInt(container.css('margin-top'))
-    });
-
-
-
-    controlDrag();
-
-
-    function controlDrag() {
-      let startX, startY, left, top, rate;
-      container
-        .on('mousedown', (e) => {
-          if (dragging) return;
-          dragging = {
-            self: container,
-            rate: 0.75
-          };
-        })
-        .on('mouseup mouseleave', () => {
-          if (!dragging) return;
-          let left = container.css('left').slice(0, -2),
-              top = container.css('top').slice(0, -2);
-          left = Math.min(Math.max(left, frame.width() - container.outerWidth(true)), 0);
-          top = Math.min(Math.max(top, frame.height() - container.outerHeight(true)), 0);
-          container.css('left', left + 'px');
-          container.css('top',  top + 'px');
-        });
-
-      frame
-        .on('mousedown', (e) => {
-          if (!dragging) return;
-          e = dragging.e || e;
-          e.preventDefault();
-
-          left = dragging.self.position().left;
-          top = dragging.self.position().top;
-          startX = e.pageX;
-          startY = e.pageY;
-          rate = dragging.rate || 1;
-          dragging.self.addClass('active');
-        })
-        .on('mousemove', (e) => {
-          if (!dragging) return;
-          e.preventDefault();
-
-          let dX = e.pageX - startX,
-              dY = e.pageY - startY;
-          dragging.self.css('left', dX * rate + left + 'px');
-          dragging.self.css('top',  dY * rate + top + 'px');
-        })
-        .on('mouseup mouseleave', (e) => {
-          if (!dragging) return;
-          e.preventDefault();
-
-          dragging.self.removeClass('active');
-          dragging = false;
-        });
-    }
-
-    //
-
+      loadEntires();
+      controlDrag();
   });
 })();
